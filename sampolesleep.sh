916cb8cad9ad23c79f10fc0ff279b97f72335a5c
VikashKumarX <129828854+VikashKumarX@users.noreply.github.com>
2023-04-28 12:26:41 +0530

    Update sampolesleep.sh

--- sampolesleep.sh
+++ sampolesleep.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
-
+#Purpose: Sleep  Demo
+#Version:1.0
+#Created Date: Fri 28 Apr 
+#Author: Vikash Kumar
+# START #
 Delay=$1
 echo "we going to sleep"
 sleep $Delay
 echo "awake"
+# END #
