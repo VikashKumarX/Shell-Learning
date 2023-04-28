c580346c2ae8fcd7a772b6656563ce9a4ff303b4
VikashKumarX <129828854+VikashKumarX@users.noreply.github.com>
2023-04-28 12:54:02 +0530

    Create SampleQutoDiffer.sh

---
+++ SampleQutoDiffer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Purpose: Verifying Difference between quotation marks
+#Version: 1.0
+#Created Date: 2023 Apr
+#Author: Vikash Kumar
+# START #
+VAR1=123456
+TEST=Demoi
+
+# Double Quotes
+echo "Execute double quotes $VAR1 $TEST"
+
+# Single Quotes
+echo 'Excute Single Quotes $VAR1 $TEST'
+
+# Reverse Quotes
+echo "This Hostname is: `cal`"
+
+# END #
